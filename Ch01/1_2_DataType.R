52c621628e9c5f2ee96c8d6126e09b7643b53d5d
a3539a <a3539a@gmail.com>
2021-06-28 17:06:09 +0900

    2021/06/28 R Ch01 R 설치 및 개요 실습예제

---
+++ Ch01/1_2_DataType.R
@@ -0,0 +1,30 @@
+# 날짜 : 2021/06/28
+# 이름 : 김승용
+# 내용 : ch01. R설치와 개요 - 데이터 타입 교재 p41
+
+# 숫자형
+num1 <- 1
+num2 <- 2
+num3 <- 3.14
+num1
+num2;num3
+
+# 논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1;bool2
+
+# 문자형
+str1 <- "Hello"
+str2 <- 'Welcome'
+str1
+str2
+
+# 특수형
+data1 <- NULL # 초기값을 의미
+data2 <- NA   # 결측값, Not Available
+data3 <- NaN  # 숫자가 아닌 값, Not a Number
+
+data1
+data2
+data3
